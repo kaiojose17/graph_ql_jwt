6f52426d20575feda8c94293906d3f3ca1c5c549
Jose Kaio Silva dos Santos <kaio_jose96@hotmail.com>
2019-09-01 23:43:15 -0300

    Construindo Api com GraphQL

---
+++ APIProdutos/Models/GraphQL/ProdutoType.cs
@@ -0,0 +1,17 @@
+using GraphQL.Types;
+
+namespace APIProdutos.Models.GraphQL
+{
+    public class ProdutoType : ObjectGraphType<Produto>
+    {
+        public ProdutoType()
+        {
+            Name = "Produto";
+
+            Field(x => x.CodigoBarras).Description("Código de barras");
+            Field(x => x.Nome).Description("Nome do produto");
+            Field(x => x.Preco).Description("Preço");
+        }
+        
+    }
+}
---
+++ APIProdutos/Queries/EatMoreQuery.cs
@@ -0,0 +1,23 @@
+using APIProdutos.Data;
+using APIProdutos.Models.GraphQL;
+using GraphQL.Types;
+
+namespace APIProdutos.Queries
+{
+    public class EatMoreQuery : ObjectGraphType
+    {
+        public EatMoreQuery(ApplicationDbContext db)
+        {
+            Field<ListGraphType<ProdutoType>>(
+                "produtos",
+                resolve: context =>
+                {
+                    var produtos = db
+                        .Produtos;
+
+                    return produtos;
+                });
+        }
+        
+    }
+}
